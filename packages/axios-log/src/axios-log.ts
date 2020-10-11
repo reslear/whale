f2f932802cbc3215b39388125c34da73db8a3020
reslear <reslear@gmail.com>
2020-10-11 06:40:01 +0300

    refactor: URLSearchParams

--- packages/axios-log/src/axios-log.ts
+++ packages/axios-log/src/axios-log.ts
@@ -8,6 +8,9 @@ export const axiosLog = (config: AxiosRequestConfig) => {
         .join(chalk.yellow("&"))
     : "";
 
+  // TODO: refactor
+  let urlParams = new URLSearchParams(config.params);
+
   let time = new Date().toLocaleTimeString();
   let method = config.method ?? "";
   let url = new URL(config.url, config.baseURL).href;
