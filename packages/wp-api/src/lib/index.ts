7fe7df56a5226dd28fb1a3218a214e3d0f275a50
reslear <reslear@gmail.com>
2021-01-12 09:01:16 +0300

    refactor(wp-api): add class

--- packages/wp-api/src/lib/index.ts
+++ packages/wp-api/src/lib/index.ts
@@ -1,1 +1,94 @@
-export {};
+import axios, { AxiosRequestConfig, AxiosInstance, Method } from "axios";
+
+export class WordPressApi {
+  prefix = "/wp-json/wp/v2";
+  axios: AxiosInstance;
+
+  constructor(axiosConfig?: AxiosRequestConfig) {
+    this.axios = axios.create(axiosConfig);
+  }
+
+  async request(
+    url: string,
+    data: any = {},
+    method: Method = "get",
+    config: AxiosRequestConfig = {}
+  ) {
+    const params = {
+      [method.toLowerCase() === "get" ? "params" : "data"]: data,
+    };
+
+    try {
+      const result = await this.axios({
+        method,
+        url: `${url}`,
+        ...params,
+        ...config,
+      });
+
+      return result;
+    } catch (e) {
+      if (e.response) {
+        console.log(e.response.data);
+        console.log(e.response.status);
+        console.log(e.response.headers);
+      } else if (e.request) {
+        console.log(e.request);
+      } else {
+        console.log("Error", e.message);
+      }
+      console.log(e);
+    }
+  }
+
+  async get(
+    url: string,
+    data: any = {},
+    options: {
+      headers?: boolean;
+      prefix?: string;
+    } = {}
+  ) {
+    const req = await this.request((options.prefix ?? this.prefix) + url, data);
+
+    if (!req) return null;
+
+    if (options.headers) {
+      return {
+        headers: req.headers,
+        data: req.data,
+      };
+    } else {
+      return req.data;
+    }
+  }
+
+  async post(
+    url: string,
+    data: any = {},
+    options: {
+      headers?: boolean;
+      prefix?: string;
+    } = {}
+  ) {
+    const req = await this.request(
+      (options.prefix ?? this.prefix) + url,
+      data,
+      "post",
+      {
+        headers: { "content-type": "application/x-www-form-urlencoded" },
+      }
+    );
+
+    if (!req) return null;
+
+    if (options.headers) {
+      return {
+        headers: req.headers,
+        data: req.data,
+      };
+    } else {
+      return req.data;
+    }
+  }
+}
