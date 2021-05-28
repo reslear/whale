82453fedc8c38d83ef571842ac4c04352c27af54
reslear <reslear@gmail.com>
2021-05-28 23:09:49 +0300

    Delete packages/vue-naked-components/test/play/nuxt/.nuxt directory

--- packages/vue-naked-components/test/play/nuxt/.nuxt/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as MiniCarousel } from '../../../../../src/components/MiniCarousel.vue'
-
-export const LazyMiniCarousel = import('../../../../../src/components/MiniCarousel.vue' /* webpackChunkName: "components/mini-carousel" */).then(c => c.default || c)
